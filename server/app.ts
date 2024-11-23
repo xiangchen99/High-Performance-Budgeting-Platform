865f4370644c8fd15a51647a38dd5b89875c184f
Xiang <xianc@seas.upenn.edu>
2024-11-23 14:03:35 -0500

    sheesh

--- server/app.ts
+++ server/app.ts
@@ -2,10 +2,16 @@ import { Hono } from 'hono'
 import {logger} from 'hono/logger'
 import { expensesRoute } from './routes/expenses'
 import { serveStatic } from 'hono/bun'
+import {cors} from 'hono/cors'
 
 const app = new Hono()
 
 app.use(logger())
+
+app.use(cors({
+    origin: 'https://expense-tracker-frontend-hr18.onrender.com/', // Replace with your frontend's domain
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'], // Customize methods as needed
+  }))
 
 app.route("/api/expenses", expensesRoute)
 
