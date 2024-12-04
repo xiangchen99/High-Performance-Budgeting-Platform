24cc98a228e05775b69b7dfba093580b2c0e2a9e
Xiang chen <xianc@seas.upenn.edu>
2024-12-03 20:42:00 -0500

    added cookie login

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -3,10 +3,10 @@ import {kindeClient, sessionManager} from '../kinde'
 
 export const authRoute = new Hono()
 .get("/login", async (c) => {
-    const loginUrl = await kindeClient.login(sessionManager);
+    const loginUrl = await kindeClient.login(sessionManager(c));
     return c.redirect(loginUrl.toString());
   })
 .get("/register", async (c) => {
-    const registerUrl = await kindeClient.register(sessionManager);
+    const registerUrl = await kindeClient.register(sessionManager(c));
     return c.redirect(registerUrl.toString());
   });
